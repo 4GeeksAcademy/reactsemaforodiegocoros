ada2d3f050976b13bb53cd4c82cb61db91e31597
Diegocoros <diegocoros22@gmail.com>
2024-11-25 20:01:38 +0000

    First commit

---
+++ src/components/DefaultComponent.jsx
@@ -0,0 +1,8 @@
+const DefaultComponent = () => {
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  );
+};
+export default DefaultComponent;
---
+++ src/pages/DefaultPages.jsx
@@ -0,0 +1,8 @@
+const DefaultPages = () => {
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  );
+};
+export default DefaultPages;
